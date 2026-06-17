75b9024fbe4a17a4fe13d3c6f8135331aeb0df1a
Abhinav_Dubey <abhinavdubey1302@gmail.com>
2026-06-17 20:42:32 +0530

    Time: 7 ms (46.85%) | Memory: 43 MB (70.08%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> count = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        int maxLen = 0;
+        boolean odd = false;
+
+        for (int freq : count.values()) {
+            if (freq % 2 == 0) {
+                maxLen += freq;
+            } else {
+                maxLen += freq - 1;
+                odd = true;
+            }
+        }
+
+        return maxLen + (odd ? 1 : 0);
+    }
+}
